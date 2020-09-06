577acc56b97214098be185751fc3dd95992b5268
leuri <leouri2001@gmail.com>
2020-09-06 19:20:27 +0300

    added first task

---
+++ lab1/default/01.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This is the task number I in lab 01
+
+if [[ "$1" -gt "$2" -a "$1" -gt "$3" ]]
+then echo "$1"
+else	if [[ "$2" -gt "$1" -a "$2" -gt "$3" ]]
+	then echo "$2"
+	else echo "$3"
+	fi
+fi
+exit 0
+
