b89ea363a6e36b3af35294601192a5d387e5781a
leuri <leouri2001@gmail.com>
2020-09-07 12:48:50 +0300

    First task fixed

--- lab1/default/01.bash
+++ lab1/default/01.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This is the task number I in lab 01
 
-if [[ "$1" -gt "$2" -a "$1" -gt "$3" ]]
+if [ "$1" -gt "$2" ] && [ "$1" -gt "$3" ]
 then echo "$1"
-else	if [[ "$2" -gt "$1" -a "$2" -gt "$3" ]]
+else	if [ "$2" -gt "$1" ] && [ "$2" -gt "$3" ]
 	then echo "$2"
 	else echo "$3"
 	fi
