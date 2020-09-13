bc427c1dcea64ee6ee52db667f359471e25b181a
leuri <leouri2001@gmail.com>
2020-09-13 16:56:38 +0300

    Fixed error handling

--- lab1/other/modules/search.bash
+++ lab1/other/modules/search.bash
@@ -4,7 +4,12 @@ if [[ $# -eq 2 ]]
 then
 	if [[ -d "$2" ]]
 	then
-	echo "$(grep -r "$1" "$2")"
+		result="$(grep -r "$1" "$2")"
+		if [[ $? -ne 0 ]]
+		then exit 6 #in case of error of other programm return 6
+		else echo "$result"
+		exit 0
+		fi
 	else exit 5
 	fi
 else exit 1
