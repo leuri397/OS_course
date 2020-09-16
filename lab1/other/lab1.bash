c529ab6d6a0353556c80b9b884d431f2e09c09fb
leuri <leouri2001@gmail.com>
2020-09-16 20:47:13 +0300

    style: add tabs and newlines

--- lab1/other/lab1.bash
+++ lab1/other/lab1.bash
@@ -5,8 +5,10 @@ FILEPATH="$(dirname -- "$(realpath "$0")")"
 function help {
 HELPPATH="$FILEPATH"
 if [[ "$#" -eq 0 ]]
-then echo "$(cat $HELPPATH/help)"
-else echo "$(cat $HELPPATH/modules/$1_help)"
+then
+	echo "$(cat $HELPPATH/help)"
+else
+	echo "$(cat $HELPPATH/modules/$1_help)"
 fi
 }
 
@@ -20,17 +22,23 @@ then
 	exit 0
 else
 	case $return_code in
-	1) echo "Wrong amount of arguments" >&2
+	1)
+		echo "Wrong amount of arguments" >&2
 	;;
-	2) echo "Wrong meaning of arguments" >&2
+	2)
+		echo "Wrong meaning of arguments" >&2
 	;;
-	3) echo "Imposiible operation" >&2
+	3)
+		echo "Imposiible operation" >&2
 	;;
-	4) echo "Unknown argument" >&2
+	4)
+		echo "Unknown argument" >&2
 	;;
-	5) echo "Directory or file does not exists" >&2
+	5)
+		echo "Directory or file does not exists" >&2
 	;;
-	6) echo "Internal comand error" >&2
+	6)
+		echo "Internal comand error" >&2
 	;;
 	esac
 	echo "$(help $1)"
@@ -42,64 +50,72 @@ function interactive_start {
 possible_actions=("${existing_module_list[@]}"  "help" "exit" )
 while :
 do
-echo "Choose one of the options below by typing its number or name:"
-index="0"
+	echo "Choose one of the options below by typing its number or name:"
+	index="0"
 	for module in ${possible_actions[@]}
 	do
-	echo "$index. $module"
-	index=$(($index + 1))
-	done
-read option
-if [[ $option =~ ^[0-9]+$ ]]
-then
-	if [[ $option -lt ${#possible_actions[@]} ]] && [[ $option -ge 0 ]]
-	then module=${possible_actions[$option]}
-	else echo "Invalid option. Try again"
-	continue
+		echo "$index. $module"
+		index=$(($index + 1))
+		done
+	read option
+	if [[ $option =~ ^[0-9]+$ ]]
+	then
+		if [[ $option -lt ${#possible_actions[@]} ]] && [[ $option -ge 0 ]]
+		then
+			module=${possible_actions[$option]}
+		else
+			echo "Invalid option. Try again"
+		continue
+		fi
+	else
+		if [[ ${possible_actions[@]} =~ ("$option") ]]
+		then
+			module="$option"
+		else
+			echo "Unknown option. Try again"
+			continue
+		fi
 	fi
-else
-	if [[ ${possible_actions[@]} =~ ("$option") ]]
-	then module="$option"
-	else echo "Unknown option. Try again"
-	continue
+	no_arg=( "log" "exit" "help")
+	if ! [[ ${no_arg[@]} =~ ("$module") ]]
+	then
+		echo "Enter arguments"
+	read arg1 arg2 arg3 arg4
 	fi
-fi
-no_arg=( "log" "exit" "help")
-if ! [[ ${no_arg[@]} =~ ("$module") ]]
-then echo "Enter arguments"
-read arg1 arg2 arg3 arg4
-fi
-case $module in
-"exit")
-	exit 0
-	;;
-help)
-	help
-	;;
-calc|search|reverse|log)
-	echo "$(module_call $module $arg1 $arg2 $arg3 $arg4)"
-	continue
-	;;
-esac
+	case $module in
+	"exit")
+		exit 0
+		;;
+	help)
+		help
+		;;
+	calc|search|reverse|log)
+		echo "$(module_call $module $arg1 $arg2 $arg3 $arg4)"
+		continue
+		;;
+	esac
 done
 }
 
 if ! [[ -e "$FILEPATH/modules_list" ]]
-then echo "Missing list of modules. Program is terminating">&2
-exit -1
+then
+	echo "Missing list of modules. Program is terminating">&2
+	exit -1
 else
 	for module in $(cat $FILEPATH/modules_list)
 	do
 		if [[ -e "$FILEPATH/modules/$module.bash" ]]
 		then
-		existing_module_list+=( "$module" )
+			existing_module_list+=( "$module" )
 		else
-		missing_module_list+=( "$module" )
+			missing_module_list+=( "$module" )
 		fi
 	done
 fi
+
 if ! [[ -z "$missing_module_list" ]]
-then echo "There are some missing modules: ${missing_module_list[@]}">&2
+then
+	echo "There are some missing modules: ${missing_module_list[@]}">&2
 fi
 
 if [[ $# -eq 0 ]]
@@ -107,20 +123,26 @@ then
 	help
 	exit -2
 fi
+
 case $1 in
 calc|search|reverse|strlen|log)
 	if [[ $# == 1 ]] && [[ $1 != "log" ]]
-	then help $1 
-	exit -2
+	then
+		help $1 
+		exit -2
 	fi
 	if [[ ${existing_module_list[@]} =~ ($1) ]]
-	then result="$(module_call $1 "$2" $3 $4 $5 $6)"
+	then
+		result="$(module_call $1 "$2" $3 $4 $5 $6)"
 		if [[ $? -eq 0 ]]
-		then echo "$result"
-		else exit -2
+		then
+			echo "$result"
+		else
+			exit -2
 		fi
-	else echo "Attempt to call non-existing module">&2
-	exit -2
+	else
+		echo "Attempt to call non-existing module">&2
+		exit -2
 	fi
 	;;
 "exit")
